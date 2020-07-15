6c6246529ec018326c3287139b9e917c32c31fbf
Jamie Kydd <jamie.b.kydd@gmail.com>
2020-07-15 17:28:37 +1200

    Add project from default Android Studio Native C++ template

---
+++ app/src/main/java/com/android/samples/repro/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.android.samples.repro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+    }
+
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
--- main.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include <iostream>
-
-int main() {
-	using namespace std;
-	cout << "Hello CMake." << endl;
-	return 0;
-}
